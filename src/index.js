239522a0958bbc9936909a23b30840e3a65775d6
Roland Doda <rolanddoda2014@gmail.com>
2021-09-24 20:33:17 +0200

    Sum numbers even if when they are in different files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import { getFile } from './utils/get-file'
 import * as h from './utils/helpers'
 
-async function getSumsOfFiles(filePath) {
+async function getSumsOfFiles(filePath, sumsObj) {
   const file = await getFile([filePath])
   const linesOfFile = file.split(/\r?\n/)
   let sum = 0
 
   for (const line of linesOfFile) {
     if (h.isNumber(line)) sum += +line
-    else sum += await getSumsOfFiles(line)
+    else {
+      const fileSumObj = await getSumsOfFiles(line, sumsObj)
+      sum += +fileSumObj[line]
+    }
   }
 
-  return sum
+  sumsObj[filePath] = sum
+
+  return sumsObj
 }
 
 export async function cli(argv) {
   const args = argv.slice(2)
-  const sums = await getSumsOfFiles(args[0])
+  const sums = await getSumsOfFiles(args[0], {})
 
   console.log(sums)
 }
