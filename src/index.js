6287758726923fd09741543d06b0c327b37c7161
Roland Doda <rolanddoda2014@gmail.com>
2021-09-25 10:16:58 +0200

    Output files by the order they got accessed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { getFile } from './utils/get-file'
 import { validateArgs } from './utils/validate-args'
 import path from 'path'
 
-async function getSumsOfFiles(filename, sumsObj, cmdDirectory) {
+async function getSumsOfFiles(filename, cmdDirectory, sumsObj = {}, fileAccess = 0) {
   const file = await getFile(cmdDirectory + '/' + filename)
   const linesOfFile = file.split(/\r?\n/)
   let sum = 0
@@ -12,12 +12,12 @@ async function getSumsOfFiles(filename, sumsObj, cmdDirectory) {
     if (h.isNumber(line)) sum += +line
     else {
       const filePath = line.trim()
-      const fileSumObj = await getSumsOfFiles(filePath, sumsObj, cmdDirectory)
-      sum += +fileSumObj[filePath]
+      const fileSumObj = await getSumsOfFiles(filePath, cmdDirectory, sumsObj, fileAccess + 1)
+      sum += +fileSumObj[filePath].sum
     }
   }
 
-  sumsObj[filename] = sum
+  sumsObj[filename] = { sum, fileAccess: fileAccess }
 
   return sumsObj
 }
@@ -28,9 +28,10 @@ export async function cli(argv) {
 
   const pwdDirectory = path.dirname(args[0]).split(path.sep).pop()
   const filename = path.basename(args[0])
-  const sums = await getSumsOfFiles(filename, {}, pwdDirectory)
+  const sums = await getSumsOfFiles(filename, pwdDirectory)
+  const sumsArraySorted = Object.entries(sums).sort((a, b) => a[1].fileAccess - b[1].fileAccess)
 
-  for (const [fileName, sum] of Object.entries(sums)) {
+  for (const [fileName, { sum }] of sumsArraySorted) {
     console.log(`${fileName} - ${sum}`)
   }
 }
